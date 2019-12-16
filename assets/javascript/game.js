5e83cacf6bf968ca0efa5e25b49444d4d4d4b62f
phil cowan <philipjcowan1@gmail.com>
2019-12-16 13:29:55 -0700

    creates the variables that hold refs to the html elements where we will display things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,11 @@ var guessesLeft = 12;
 var guessedSoFar = [""];
 var blankSpaces = [""];
 
+// Create variables that hold references to the places in the HTML where we want to display things.
+var guessedSoFarText = document.getElementById("guessedsofartext");
+var winsText = document.getElementById("win-number");
+var lossesText = document.getElementById("loss-number");
+var guessesLeftText = document.getElementById("guessesleftnumber");
 
 //Makes an array for the random word that the computer will choose
 var randomWord = ["dale", "andy", "laura", "audrey", "ben", "harry", "pete", "donna", "leland", "james", "bobby", "mike", "bob"];
@@ -21,7 +26,7 @@ console.log(blankSpaces);
 // var gameBoard = document.getElementById("gameboard")
 
 document.getElementById("gameboard").innerHTML="" + blankSpaces.join(" ");
-//makes  a function to check if the guess is in fact in the string and returns a boolean
+
 
 
 
@@ -30,7 +35,7 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     console.log(userGuess);
 
-
+//makes  a function to check if the guess is in fact in the string and returns a boolean
 function isMatch() {
     var match = word.includes(userGuess);
     console.log(match);
